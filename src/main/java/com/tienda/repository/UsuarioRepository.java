035fc0f43d1c3935f8fe5e4aae980f1f9cc86310
Charlie <carlossolano95@gmail.com>
2026-03-27 13:16:46 -0600

    Semana 11

--- src/main/java/com/tienda/repository/UsuarioRepository.java
+++ src/main/java/com/tienda/repository/UsuarioRepository.java
@@ -1,6 +1,7 @@
 package com.tienda.repository;
 
 import com.tienda.domain.Usuario;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,5 +9,18 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 
+    //Se crea una consulta derivada
     public Optional<Usuario> findByUsernameAndActivoTrue(String username);
+
+    //Se utiliza para el crud de usuarios... el listado...
+    public List<Usuario> findByActivoTrue();
+
+    //Se utiliza al modificar un usuario...
+    public Optional<Usuario> findByUsername(String username);
+
+    public Optional<Usuario> findByUsernameAndPassword(String username, String password);
+
+    public Optional<Usuario> findByUsernameOrCorreo(String username, String correo);
+
+    public boolean existsByUsernameOrCorreo(String username, String correo);
 }
